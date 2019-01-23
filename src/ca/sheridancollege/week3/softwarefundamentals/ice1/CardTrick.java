86dfd09ef577b1d50b491a1bd3581764d6dd20aa
cnrtks <cnrtks@gmail.com>
2019-01-23 16:12:26 -0500

    Added @modified by conor tompkins

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @modified by conor tompkins
  */
 public class CardTrick {
     
