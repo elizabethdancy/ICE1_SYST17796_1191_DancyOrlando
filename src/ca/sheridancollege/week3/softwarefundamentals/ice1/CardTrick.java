d24b4cc3ba01e836c69e772cac134e8de8327b4b
hensonmak <makhe@sheridancollege.ca>
2019-01-23 16:51:54 -0500

    Student code

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,6 +5,8 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -25,10 +27,22 @@ public class CardTrick {
         }
         
         //insert code to ask the user for Card value and suit, create their card
-        System.out.println("What is your card's value and suit?\n");
+        Scanner input = new Scanner(System.in);
         Card specialCard = new Card();
-        specialCard.setValue(8);
-        specialCard.setSuit(Card.SUITS[3]);
+        
+        // Get users card's value
+        System.out.print("What is your card's value?: ");
+        int specialValue = input.nextInt();
+        specialCard.setValue(specialValue);
+        
+        // Get users card's suit
+        System.out.print("What is your card's suit?: ");
+        input.next();
+        String specialSuit = input.nextLine();
+        specialCard.setSuit(specialSuit);
+        
+        System.out.println();
+       
         
         // and search magicHand here
         boolean isCard = false;
