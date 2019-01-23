2f07be8b0e489e3328f87c00afe2bbce99fe3eef
ronak1 <ronak.sheth@sheridancollege.ca>
2019-01-23 15:41:13 -0500

    new branch created for ICE 1 in ms dancy's classs commit for card trick

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -17,16 +17,17 @@ public class CardTrick {
     {
         Card[] magicHand = new Card[7];
         
-        for (int i=0; i<magicHand.length; i++)
-        {
+        //for (int i=0; i<magicHand.length; i++)
+       // {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-        }
+            c.setValue(2);
+            c.setSuit(Card.SUITS[2]);
+       // }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+        System.out.println("Cra is "+ c.getSuit() + c.getValue());
     }
     
 }
