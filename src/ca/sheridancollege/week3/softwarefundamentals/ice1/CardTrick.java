2878889ca56176280090c8c6e8a87dee0bd8db92
laufra <laufra@sheridancollege.ca>
2019-01-23 16:30:34 -0500

    Added code to CardTrick

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,11 +5,15 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @modifier Frankie Lau, 2019
  */
 public class CardTrick {
     
@@ -20,8 +24,27 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int)Math.random()*13);
+            c.setSuit(Card.SUITS[((int)Math.random()*3)]);
+            magicHand[i] = c;
+        }
+        
+        System.out.println("Please enter a card value: ");
+        Scanner sc = new Scanner(System.in);
+        int cardValue = sc.nextInt();
+        System.out.println("Please enter a suit: ");
+        Scanner sc_suit = new Scanner(System.in);
+        String givenSuit = sc_suit.nextLine();
+        
+        for(int i = 0; i < magicHand.length; i++){
+            if(cardValue == magicHand[i].getValue() && 
+                    givenSuit == magicHand[i].getSuit()){
+                System.out.println("Its your lucky card");
+            }
+            else{
+                System.out.println(magicHand[i].getValue() 
+                        + magicHand[i].getSuit());
+            }
         }
         
         //insert code to ask the user for Card value and suit, create their card
