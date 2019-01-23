28ed9d10c5a7837325d0ffee27f34a6b5c677c9f
lazerfrog14 <dcruzjer@sheridancollege.ca>
2019-01-23 16:00:46 -0500

    This is a simple card game in java.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -11,22 +11,53 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
  */
+import java.util.Scanner;
+import java.util.Random;
 public class CardTrick {
     
     public static void main(String[] args)
     {
+        Random r = new Random();
+        Scanner in = new Scanner(System.in);
         Card[] magicHand = new Card[7];
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            int val = r.nextInt(12)+1;
+            c.setValue(val);
+            
+            String suit = Card.SUITS[r.nextInt(3)];
+            c.setSuit(suit);
+            //c.setSuit(Card.SUITS[insert call to random number between 
+            //0-3 here])
         }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+        System.out.println("Enter the Card value:");
+        int value = in.nextInt();
+        
+        System.out.println("Enter the Card suit:");
+        String cardSuit = in.nextLine();
+        
+        //checks if user has a magic hand
+        for(int i = 0; i < magicHand.length; i++) {
+            if(magicHand[i].getValue() == value && 
+                    magicHand[i].getSuit().equalsIgnoreCase(cardSuit)) 
+            {
+                System.out.println("Congratulations you "
+                        + "win! You have a magic hand");
+            }
+            
+            else {
+                System.out.println("Sorry you lose. "
+                        + "You did not get a magic hand.");
+            }
+        }//end of for loop
+                
+                
     }
     
 }
