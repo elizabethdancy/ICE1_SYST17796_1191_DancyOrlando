70d3bb65d2e118a813afdb37e09084646704daf6
sommerarruda <arrudaso@sheridancollege.ca>
2019-01-23 16:31:37 -0500

    Checked to see if the card the user inputted is in the hand. If so, they win. If not, they lose.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,28 +5,60 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * modifiedBy:Sommer Arruda
  */
+import java.util.Scanner;
 public class CardTrick {
     
     public static void main(String[] args)
     {
+        Scanner keyIn = new Scanner(System.in);
+        
         Card[] magicHand = new Card[7];
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int)(Math.random()*(13) + 1));
+            c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
+            magicHand[i] = c;
+        }
+        
+        System.out.println("Input card number");
+        int cardNumber = keyIn.nextInt();
+        System.out.println("Input card suit ");
+        String cardSuit = keyIn.next();
+        
+        Card userCard = new Card();
+        userCard.setValue(cardNumber);
+        userCard.setSuit(cardSuit);
+        
+        boolean didYouWin = false;
+        for(int j = 0; j<magicHand.length; j++){
+            if((cardNumber == magicHand[j].getValue()) && 
+                    (cardSuit.equalsIgnoreCase(magicHand[j].getSuit()))){
+                didYouWin = true;
+                break;
+            }
+        }
+        
+        if(didYouWin == true){
+            System.out.println("You win");
+        }else{
+            System.out.println("You lose");
         }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+
     }
     
 }
