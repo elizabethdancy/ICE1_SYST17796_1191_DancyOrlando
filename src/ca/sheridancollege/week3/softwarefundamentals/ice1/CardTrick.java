7457c106c5575f73696535b44f543787b2c2b78d
hensonmak <makhe@sheridancollege.ca>
2019-01-23 16:21:42 -0500

    Student code

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -16,15 +16,16 @@ public class CardTrick {
     
     public static void main(String[] args){
         Card[] magicHand = new Card[7];
-        Card c = new Card();
         
         for (int i = 0; i < magicHand.length; i++){
-            c.setValue((int)(Math.random() * 8));
+            Card c = new Card();
+            c.setValue((int)(Math.random() * 14));
             c.setSuit(Card.SUITS[(int)(Math.random() * 4)]);
+            magicHand[i] = c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
-        System.out.println("What is your card's value and suit?");
+        System.out.println("What is your card's value and suit?\n");
         Card specialCard = new Card();
         specialCard.setValue(8);
         specialCard.setSuit(Card.SUITS[3]);
@@ -32,21 +33,22 @@ public class CardTrick {
         // and search magicHand here
         boolean isCard = false;
         for (int i = 0; i < magicHand.length; i++) {
-            if(magicHand[i] == specialCard){
+            if(magicHand[i].getValue() == specialCard.getValue() && magicHand[i].getSuit().equals(specialCard.getSuit())){
                 isCard = true;
             }
         }
         //Then report the result here
         for (int i = 0; i < magicHand.length; i++) {
-            String format = "Magic hand contains:%n"
-                    + "%s of %s %n";
+            String format = "%d of %s %n";
+            
             System.out.printf(format, magicHand[i].getValue(), magicHand[i].getSuit());
+            
         }
         
         if(isCard == true)
-            System.out.println("The magic hand contains your card!");
+            System.out.printf("%nThe magic hand contains your card!%nYour card was %d of %s%n", specialCard.getValue(), specialCard.getSuit());
         else
-            System.out.println("The magic hand does not contain your card!");
+            System.out.printf("%nThe magic hand does not contain your card!%nYour card was %d of %s%n", specialCard.getValue(), specialCard.getSuit());
         
     }
     
