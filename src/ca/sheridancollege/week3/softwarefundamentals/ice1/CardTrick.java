e451baac91d5ca437e89bb2879b4bded289e88ff
hensonmak <makhe@sheridancollege.ca>
2019-01-23 15:48:24 -0500

    Student code

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -14,20 +14,35 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  */
 public class CardTrick {
     
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
         Card[] magicHand = new Card[7];
+        Card c = new Card();
         
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+        for (int i = 0; i < magicHand.length; i++){
+            c.setValue((int)(Math.random() * 8));
+            c.setSuit(Card.SUITS[(int)(Math.random() * 4)]);
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        System.out.println("What is your card's value and suit?");
+        Card specialCard = new Card();
+        specialCard.setValue(8);
+        specialCard.setSuit(Card.SUITS[3]);
+        
         // and search magicHand here
+        boolean isCard = false;
+        for (int i = 0; i < magicHand.length; i++) {
+            if(magicHand[i] == specialCard){
+                isCard = true;
+            }
+        }
         //Then report the result here
+        
+        if(isCard == true)
+            System.out.println("The magic hand contains your card!");
+        else
+            System.out.println("The magic hand does not contain your card!");
+        
     }
     
 }
