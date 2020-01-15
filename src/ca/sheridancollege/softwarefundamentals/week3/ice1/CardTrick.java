ab697f259ecb7d0c231423751fb508227a272aa0
elizabethdancy <elizabeth.dancy@sheridancollege.ca>
2020-01-15 10:39:46 -0500

    new branch

--- src/ca/sheridancollege/softwarefundamentals/week3/ice1/CardTrick.java
+++ src/ca/sheridancollege/softwarefundamentals/week3/ice1/CardTrick.java
@@ -5,6 +5,8 @@
  */
 package ca.sheridancollege.softwarefundamentals.week3.ice1;
 
+import java.util.Random;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -19,10 +21,13 @@ public class CardTrick {
         
         for (int i=0; i<magicHand.length; i++)
         {
+            Random r = new Random();
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-        }
+            c.setValue(r.nextInt(13)+1);
+            c.setSuit(Card.SUITS[r.nextInt(4)]);
+            magicHand[i] = c;
+            System.out.println(magicHand[i].getValue() + " " + magicHand[i].getSuit());
+            }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
